c0313842914b633f5443e5d1fda25443ba83fab2
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-03-15 12:03:57 +0300

    Created models for db and configured django admin

--- fuseapp/fuseapp/urls.py
+++ fuseapp/fuseapp/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, contact
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
